837ef241599a72a33421b05061b768d4d16e9ba3
Unknown <luton66@gmail.com>
2019-03-31 19:28:05 +0100

    A much simpler way of displaying current directories and containing files
    I've also added a System.out.println output that explains what's going on.

--- ServerApplication/src/main/java/ClientServerApplication/ServerApplication/ServerApplication.java
+++ ServerApplication/src/main/java/ClientServerApplication/ServerApplication/ServerApplication.java
@@ -3,13 +3,10 @@ package ClientServerApplication.ServerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -17,11 +14,29 @@ public class ServerApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ServerApplication.class, args);
 
-		Stream<Path> walk = Files.walk(Paths.get("C:\\Gradle"));
+//		Want to investigate this. The use of Files.walk seems to be a recursive action, that will
+//		get the details of all files in all folders.
+//		Stream<Path> walk = Files.walk(Paths.get("C:\\Gradle"));
+//		Stream<Path> walk2 = walk.filter(Files::isRegularFile);
+//		List<String> result = walk2.map(Object::toString).collect(Collectors.toList());
 
-		Stream<Path> walk2 = walk.filter(Files::isRegularFile);
+		File directory = new File(".");
 
-		List<String> result = walk2.map(Object::toString).collect(Collectors.toList());
+		System.out.println("Current Directory : " + directory.getCanonicalFile().getName() + "\n");
+
+		List<File> files = Arrays.asList(directory.listFiles());
+
+		System.out.println("Simply listing the files as files.toString will result in");
+		files.forEach(file -> System.out.println(file.toString()));
+
+		System.out.println("\nHowever, if we want to remove the .\\ Then we need to use the CanonicalFile details, as such.");
+		files.forEach(file -> {
+			try {
+				System.out.println(file.getCanonicalFile().getName());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 
 		String x = "x";
 	}
